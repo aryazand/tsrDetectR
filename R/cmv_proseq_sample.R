0ee54bb359c790fe1b540306d113b3ce44cf98f8
Arya Zandvakili <{ID}+{username}@users.noreply.github.com>
2025-02-21 16:33:55 -0600

    added a sample bed file derived from pro-seq

---
+++ R/cmv_proseq_sample.R
@@ -0,0 +1,9 @@
+#' Sample PRO-seq dataset
+#'
+#' Sample BED file derived from a PRO-seq dataset measure nascent RNA transcribed from the CMV genome during an *in vitro* CMV infection of fibroblast cells
+#'
+#' @format ## `cmv_proseq_sample`
+#' A GRanges objects with 7876 fragments on the CMV genome (Genbank KF297339.1)
+#'
+#' @source derived from NCBI's SRA data SRR16301852
+"cmv_proseq_sample"
