02b33f72e29fa8d31767757602c05c90152e9f8a
Arya Zandvakili <{ID}+{username}@users.noreply.github.com>
2025-08-04 14:04:08 -0500

    remove unnecessary sample data

--- data-raw/cmv_proseq_hff_12hpi.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `cmv_proseq_hff_12hpi` dataset goes here
-cmv_proseq_hff_12hpi <- rtracklayer::import.bed(
-  "data-raw/SRR16301856_cmv.bed",
-  genome = "cytomegalovirus",
-  seqinfo = GenomeInfoDb::Seqinfo(seqnames = "KF297339.1", seqlengths = 237683))
-
-usethis::use_data(cmv_proseq_hff_12hpi, overwrite = TRUE)
--- data-raw/cmv_proseq_sample.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `cmv_proseq_sample` dataset goes here
-cmv_proseq_sample <- rtracklayer::import.bed(
-  "data-raw/SRR16301852_cmv.bed",
-  genome = "cytomegalovirus",
-  seqinfo = Seqinfo(seqnames = "KF297339.1", seqlengths = 237683))
-
-usethis::use_data(cmv_proseq_sample, overwrite = TRUE)
