da243f70f8c0b09759a23d0c7708317dd87685c8
Arya Zandvakili <{ID}+{username}@users.noreply.github.com>
2025-08-08 12:00:02 -0500

    create bam_to_grange function

--- R/bam_to_grange.R
+++ R/bam_to_grange.R
@@ -1,20 +1,38 @@
-#' Title
+#' Convert a GAlignments object to GRanges object
 #'
-#' @param x GAlignmentPairs object
+#' @param x GAlignments or GAlignmentPairs object
 #' @param keep which metadata to keep
 #'
 #' @returns GenomicRanges object
+#' @import GenomicAlignments
 #' @export
 #'
 #' @examples
-#' x <- system.file("inst", "extdata", "04_hpi_flavo_pro-seq_cmv_tb40e_sampled.bam")
-#' bam_to_grange(x, keep = "mapq")
+#' reads <- GenomicAlignments::GAlignments(
+#'   names = c("a","b","c","d","e","f","g"),
+#'   seqnames = Rle(c(rep(c("chr1", "chr2"), 3), "chr1")),
+#'   pos = c(1400, 2700, 3400, 7100, 4000, 3100, 5200),
+#'   cigar = c("500M", "100M", "300M", "500M", "300M",
+#'             "50M200N50M", "50M150N50M"),
+#'   strand = strand(rep("+", 7)),
+#'   mapq = sample(seq(42), 7))
+#'
+#' bam_to_grange(reads, keep = "mapq")
 bam_to_grange <- function(x, keep = NULL) {
 
-  gr <- granges(x, use.names = TRUE)
+  checkmate::assert(
+    checkmate::checkClass(x, "GAlignments"),
+    checkmate::checkClass(x, "GAlignmentPairs"),
+    combine = "or"
+  )
 
-  if("mapq" %in% keep) {
-    mcols(gr)$mapq <- x@first@elementMetadata$mapq
+  gr <- GenomicRanges::granges(x, use.names = TRUE)
+
+  if(!is.null(keep))
+    if(inherits(x, "GAlignments")) {
+      mcols(gr)[keep] <- x@elementMetadata[keep]
+    } else if(inherits(x, "GAlignmentPairs")) {
+      mcols(gr)[keep] <- x@first@elementMetadata[keep]
   }
 
   return(gr)
