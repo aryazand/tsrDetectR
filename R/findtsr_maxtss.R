e54fc01465823511d288ac3f817aecc2b7ec6a6c
Arya Zandvakili <28754724+aryazand@users.noreply.github.com>
2025-11-06 13:27:30 -0600

    update findtsr_maxtss

--- R/findtsr_maxtss.R
+++ R/findtsr_maxtss.R
@@ -6,13 +6,39 @@
 #' @param threshold windows are scored by the maximum value within the window. The score must be greater than threshold value for a window to be considered a TSR
 #' @param rel_threshold windows are scored by the maximum value within the window. The percentile rank of the relative (relative to all values within the Rle object) must be greater than rel_threshold for the window to be considered a TSR.
 #'
-#' @return an IRanges object of all TSR windows
+#' @return an IRanges object of all TSR windows, with metadata columns \code{score} (the maximum value within the window) and \code{background.score} (the background value for the window)
 #' @export
 #'
 #' @examples
-#' x <- strand_coverage(cmv_proseq_sample)
-#' x <- x[[1]][[1]]
-#' findtsr_maxtss(x = x, w = 21, background = 501, threshold = 10)
+#'
+#' ##################################
+#' # CREATE SAMPLE GENOME COVERAGE
+#' ##################################
+#' chr_length = 1000
+#' n_tss = 10
+#' tss_positions = sample(chr_length, n_tss)
+#' non_tss_positions = seq(chr_length)[-tss_positions]
+#'
+#' # Assume transcription starts are a poisson process for simplification
+#' avg_background_coverage = 3
+#' avg_tss_coverage = 21
+#' fiveprime_coverage <- rpois(chr_length, lambda = avg_background_coverage)
+#' fiveprime_coverage[tss_positions] <- rpois(n_tss, lambda = avg_tss_coverage)
+#' fiveprime_coverage <- Rle(fiveprime_coverage)
+#'
+#' #####################
+#' # FIND PEAKS
+#' #####################
+#' min_peak = 10
+#' peaks <- findtsr_maxtss(fiveprime_coverage, w = 21, background = 101, threshold = min_peak)
+#' predicted_tss_positions <- start(resize(peaks, width = 1, fix = "center"))
+#'
+#' plot(fiveprime_coverage, type = "l")
+#' abline(h = min_peak, lty = 2, col = "red")
+#' points(tss_positions, as.vector(fiveprime_coverage)[tss_positions], col = "red")
+#' points(predicted_tss_positions, as.vector(fiveprime_coverage)[predicted_tss_positions], pch = 20, col = "green")
+#' print(paste("Sensitivity:", sum(tss_positions %in% predicted_tss_positions)/n_tss))
+#' print(paste("Specificity:", sum(!(non_tss_positions %in% predicted_tss_positions))/length(non_tss_positions)))
 findtsr_maxtss <- function(x, w, background = 501, threshold = 0, rel_threshold = 0) {
 
   # Calculate max for each window
@@ -28,12 +54,18 @@ findtsr_maxtss <- function(x, w, background = 501, threshold = 0, rel_threshold 
   w2 = (w-1)*2 + 1
   overlap.max = S4Vectors::runq(x, k=w2, i = w2, endrule = "constant")
 
-  # Create a IRanges of the maxtss TSRs
-  maxtss.pos <- which(x == overlap.max &
-                      x > background.rle &
-                      x >= threshold &
-                      rel_x >= rel_threshold)
+  # Determine which position meet criteria for maxtss
+  is_max_overlap <- x == overlap.max
+  is_above_background <- x > background.rle
+  is_above_threshold <- x >= threshold
+  is_above_rel_threshold <- rel_x >= rel_threshold
 
+  maxtss.pos <- which(is_max_overlap &
+                      is_above_background &
+                      is_above_threshold &
+                      is_above_rel_threshold)
+
+  # Create an IRanges of maxtss
   maxtss <- IRanges::IRanges(
     start = maxtss.pos,
     width = 1,
